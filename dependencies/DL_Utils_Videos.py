c379ed34464b8df257090383148d58bab3c85a57
sebastiansanchez <cienciadatos@ultracom.com.co>
2021-07-27 15:45:05 -0500

    Adición de fución crop

--- dependencies/DL_Utils_Videos.py
+++ dependencies/DL_Utils_Videos.py
@@ -1,3 +1,10 @@
+def crop_center_square(frame):
+    y, x = frame.shape[0:2]
+    min_dim = min(y, x)
+    start_x = (x // 2) - (min_dim // 2)
+    start_y = (y // 2) - (min_dim // 2)
+    return frame[start_y : start_y + min_dim, start_x : start_x + min_dim]
+
 def load_video(path, max_frames=0, resize=(IMG_SIZE, IMG_SIZE)):
     cap = cv2.VideoCapture(path)
     frames = []
