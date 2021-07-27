75d0306ae691abf6775b4c6d6cf4408797b53dde
sebastiansanchez <cienciadatos@ultracom.com.co>
2021-07-27 15:25:12 -0500

    Video Utils Functions

---
+++ dependencies/DL_Utils_Videos.py
@@ -0,0 +1,18 @@
+def load_video(path, max_frames=0, resize=(IMG_SIZE, IMG_SIZE)):
+    cap = cv2.VideoCapture(path)
+    frames = []
+    try:
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
+            frame = crop_center_square(frame)
+            frame = cv2.resize(frame, resize)
+            frame = frame[:, :, [2, 1, 0]]
+            frames.append(frame)
+
+            if len(frames) == max_frames:
+                break
+    finally:
+        cap.release()
+    return np.array(frames)
