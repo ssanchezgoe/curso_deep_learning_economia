69505ff7b4d0d6dc66713e4c4c066db4de765a40
sebastiansanchez <cienciadatos@ultracom.com.co>
2021-07-27 15:50:30 -0500

    Correción Función Load Video

--- dependencies/DL_Utils_Videos.py
+++ dependencies/DL_Utils_Videos.py
@@ -5,7 +5,7 @@ def crop_center_square(frame):
     start_y = (y // 2) - (min_dim // 2)
     return frame[start_y : start_y + min_dim, start_x : start_x + min_dim]
 
-def load_video(path, max_frames=0, resize=(IMG_SIZE, IMG_SIZE)):
+def load_video(path, max_frames=0, resize=(224, 224)):
     cap = cv2.VideoCapture(path)
     frames = []
     try:
