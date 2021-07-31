63412116bfe613790390bcf6d9034e086d0ed1ca
ssanchezgoe <53234034+ssanchezgoe@users.noreply.github.com>
2021-07-31 11:46:48 -0500

    Update DL_Utils_Videos.py
    Actualización funciones

--- dependencies/DL_Utils_Videos.py
+++ dependencies/DL_Utils_Videos.py
@@ -1,13 +1,6 @@
 import cv2
 import numpy as np
 
-def crop_center_square(frame):
-    y, x = frame.shape[0:2]
-    min_dim = min(y, x)
-    start_x = (x // 2) - (min_dim // 2)
-    start_y = (y // 2) - (min_dim // 2)
-    return frame[start_y : start_y + min_dim, start_x : start_x + min_dim]
-
 def load_video(path, max_frames=0, resize=(224, 224)):
     cap = cv2.VideoCapture(path)
     frames = []
@@ -16,8 +9,6 @@ def load_video(path, max_frames=0, resize=(224, 224)):
             ret, frame = cap.read()
             if not ret:
                 break
-            #frame = crop_center_square(frame)
-            #frame = cv2.resize(frame, resize)
             frame = frame[:, :, [2, 1, 0]]
             frames.append(frame)
 
@@ -28,5 +19,9 @@ def load_video(path, max_frames=0, resize=(224, 224)):
     return np.array(frames)
 
 def video_info(path):
-    pass
+    seq = load_video(path)
+    print(f"Profundidad de pixel {seq.dtype}")
+    print(f"Valores (min, max): ({np.min(seq)},{np.max(seq)})")
+    print(f"Dimensiones del video: {seq.shape}")
+    print(f"Total Pixeles: {seq.ravel().shape}")
     return none
